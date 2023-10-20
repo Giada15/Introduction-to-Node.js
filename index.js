82ac97803ffe0ec7d5e999cb4b35f3c0ef3fa6e5
Giada15 <tascheriogiada@gmail.com>
2023-10-20 10:21:38 -0400

    Require and implement slugify package

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const fs = require("fs")
 const http = require("http")
 const url = require("url")
 
+const slugify = require("slugify")
+
 const replaceTemplate = require("./modules/replaceTemplate")
 
 ///////////////////////////////////////
@@ -52,6 +54,10 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
 const dataObj = JSON.parse(data)
+
+const slungs = dataObj.map((el) => slugify(el.productName, { lower: true }))
+
+console.log(slungs)
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true)
